b2d6472ad4e16a5914436420c6b447ecc1dcd96c
s21batuhankertmen <batuhankertmen@gmail.com>
2021-11-08 23:05:42 +0300

    server design

--- Server/Server/Form1.Designer.cs
+++ Server/Server/Form1.Designer.cs
@@ -29,13 +29,66 @@ namespace Server
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.text_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_listen = new System.Windows.Forms.Button();
+            this.text_msg_box = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // text_port
+            // 
+            this.text_port.Location = new System.Drawing.Point(113, 61);
+            this.text_port.Name = "text_port";
+            this.text_port.Size = new System.Drawing.Size(120, 22);
+            this.text_port.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port:";
+            // 
+            // button_listen
+            // 
+            this.button_listen.Location = new System.Drawing.Point(248, 56);
+            this.button_listen.Name = "button_listen";
+            this.button_listen.Size = new System.Drawing.Size(107, 33);
+            this.button_listen.TabIndex = 3;
+            this.button_listen.Text = "Listen";
+            this.button_listen.UseVisualStyleBackColor = true;
+            // 
+            // text_msg_box
+            // 
+            this.text_msg_box.Location = new System.Drawing.Point(72, 125);
+            this.text_msg_box.Name = "text_msg_box";
+            this.text_msg_box.Size = new System.Drawing.Size(657, 273);
+            this.text_msg_box.TabIndex = 4;
+            this.text_msg_box.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.text_msg_box);
+            this.Controls.Add(this.button_listen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text_port);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox text_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_listen;
+        private System.Windows.Forms.RichTextBox text_msg_box;
     }
 }
 
--- Server/Server/Form1.cs
+++ Server/Server/Form1.cs
@@ -16,5 +16,7 @@ namespace Server
         {
             InitializeComponent();
         }
+
+
     }
 }
